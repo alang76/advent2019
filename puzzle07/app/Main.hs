0c7d06c158ca71639284958623f89a4659508f1f
Arjen Langebaerd <alangebaerd@gmail.com>
2019-12-09 19:54:23 +0100

    Added missing file for day 7 - part 2

--- puzzle07/app/Main.hs
+++ puzzle07/app/Main.hs
@@ -37,6 +37,17 @@ data ParamModes = ParamModes {
    getThirdParam :: ParamMode
 } deriving Show
 
+data ProgramState = ProgramState {
+    getInstructionPointer :: Int,
+    getArray :: IOArray Int Int,
+    getInputs :: [Int],
+    getOutput :: Maybe Int,
+    getResult :: Maybe Int,
+    isTerminated :: Bool
+} 
+
+type Inputs = [Int] -- initial phase and following inputs
+
 trim :: String -> String
 trim = unpack . strip . pack
 
@@ -94,118 +105,143 @@ getOpCodeAndParamModes i =
    in
        (fromJust . parseMaybe opCodeParserP $ opCodeStr, fromJust . parseMaybe paramModeParserP $ paramModesStrFull)
 
-type InOut = ([Int], Maybe Int)
+processOpcodes:: ProgramState -> IO (ProgramState)
+processOpcodes programState  = do
+    let 
+        index = getInstructionPointer programState
+        array = getArray programState
+        inputs = getInputs programState
+        output = getOutput programState
+        
+    instruction <- readArray array index
+    let (opCode,paramModes) = getOpCodeAndParamModes instruction
+        
+        readParam :: Int -> IO Int
+        readParam offset = do
+            param <- readArray array (index+offset)
+            return $  param
 
-processOpcodes:: Int -> IOArray Int Int -> InOut -> IO (Int, InOut)
-processOpcodes index array inOut@(phases,resOutput) = do
-   instruction <- readArray array index
-   let (opCode,paramModes) = getOpCodeAndParamModes instruction
-       
-       readParam :: Int -> IO Int
-       readParam offset = do
-           param <- readArray array (index+offset)
-           return $  param
+        readParamVal :: Int -> ParamMode -> IO Int
+        readParamVal offset paramMode = do
+                param <- readParam offset
+                paramVal <- case paramMode of
+                        PositionMode -> readArray array param
+                        ImmediateMode -> pure param
+                return $ paramVal
 
-       readParamVal :: Int -> ParamMode -> IO Int
-       readParamVal offset paramMode = do
-               param <- readParam offset
-               paramVal <- case paramMode of
-                       PositionMode -> readArray array param
-                       ImmediateMode -> pure param
-               return $ paramVal
+        readModedParam :: Int -> IO Int
+        readModedParam idx = readParamVal idx (getParam idx paramModes)
+            where 
+                getParam idx = case idx of
+                    1 -> getFirstParam
+                    2 -> getSecondParam
+                    3 -> getThirdParam
 
-       readModedParam :: Int -> IO Int
-       readModedParam idx = readParamVal idx (getParam idx paramModes)
-           where 
-               getParam idx = case idx of
-                   1 -> getFirstParam
-                   2 -> getSecondParam
-                   3 -> getThirdParam
+        doBinOp :: (Int -> Int -> Int) -> IO ()
+        doBinOp op = do
+            left <- readModedParam 1
+            right <- readModedParam 2
+            dest <- readParam 3
+            writeArray array dest (left `op` right)
 
-       doBinOp :: (Int -> Int -> Int) -> IO ()
-       doBinOp op = do
-           left <- readModedParam 1
-           right <- readModedParam 2
-           dest <- readParam 3
-           writeArray array dest (left `op` right)
+    case {- trace ("opCode=" ++ (show opCode)) -} opCode of
+        Addition -> do
+            doBinOp (+)
+            processOpcodes $ ProgramState (index+4) array inputs output Nothing False
+        Multiplication -> do
+            doBinOp (*)
+            processOpcodes $ ProgramState (index+4) array inputs output Nothing False
+        Input -> do
+            dest <- readParam 1
+            writeArray array dest (head inputs)
+            processOpcodes $ ProgramState (index+2) array (tail inputs) output Nothing False
+        Output -> do
+            outputVal <- readParamVal 1 (getFirstParam paramModes)
+            return $ ProgramState (index+2) array inputs (Just outputVal) Nothing False
+        JumpIfTrue -> do
+            p1 <- readModedParam 1
+            p2 <- readModedParam 2
+            case p1 of 
+                0 -> processOpcodes $ ProgramState (index+3) array inputs output Nothing False
+                otherwise -> processOpcodes $ ProgramState p2 array inputs output Nothing False
+        JumpIfFalse -> do
+            p1 <- readModedParam 1
+            p2 <- readModedParam 2
+            case p1 of 
+                0 -> processOpcodes $ ProgramState p2 array inputs output Nothing False
+                otherwise -> processOpcodes $ ProgramState (index+3) array inputs output Nothing False
+        LessThan -> do
+            p1 <- readModedParam 1
+            p2 <- readModedParam 2
+            dest <- readParam 3
+            case p1 < p2 of
+                True -> writeArray array dest 1
+                False -> writeArray array dest 0
+            processOpcodes $ ProgramState (index+4) array inputs output Nothing False
+        Equals -> do
+            p1 <- readModedParam 1
+            p2 <- readModedParam 2
+            dest <- readParam 3
+            case p1 == p2 of
+                True -> writeArray array dest 1
+                False -> writeArray array dest 0
+            processOpcodes $ ProgramState (index+4) array inputs output Nothing False
+        Terminate -> do
+            result <- readArray array 1
+            return $ {- trace ("result=" ++ (show result)) -} ProgramState index array inputs output (Just result) True
 
-   case opCode of
-       Addition -> do
-           doBinOp (+)
-           processOpcodes (index+4) array inOut
-       Multiplication -> do
-           doBinOp (*)
-           processOpcodes (index+4) array inOut
-       Input -> do
-           dest <- readParam 1
-           putStrLn "input:"
-           --input <- getLine
-           writeArray array dest (head phases)
-           processOpcodes (index+2) array (tail phases, resOutput)
-       Output -> do
-           outputVal <- readParamVal 1 (getFirstParam paramModes)
-           putStrLn ("output:" ++ show outputVal)
-           processOpcodes (index+2) array (phases, Just outputVal)
-       JumpIfTrue -> do
-           p1 <- readModedParam 1
-           p2 <- readModedParam 2
-           case p1 of 
-               0 -> processOpcodes (index+3) array inOut
-               otherwise -> processOpcodes p2 array inOut
-       JumpIfFalse -> do
-           p1 <- readModedParam 1
-           p2 <- readModedParam 2
-           case p1 of 
-               0 -> processOpcodes p2 array inOut
-               otherwise -> processOpcodes (index+3) array inOut
-       LessThan -> do
-           p1 <- readModedParam 1
-           p2 <- readModedParam 2
-           dest <- readParam 3
-           case p1 < p2 of
-               True -> writeArray array dest 1
-               False -> writeArray array dest 0
-           processOpcodes (index+4) array inOut
-       Equals -> do
-           p1 <- readModedParam 1
-           p2 <- readModedParam 2
-           dest <- readParam 3
-           case p1 == p2 of
-               True -> writeArray array dest 1
-               False -> writeArray array dest 0
-           processOpcodes (index+4) array inOut
-       Terminate -> do
-           result <- readArray array 1
-           return (result, inOut)
-   
+mkAmp :: [Int] -> Int -> IO ProgramState
+mkAmp program phase = do
+    arr <- newListArray  (0, (length program)-1) program  :: IO (IOArray Int Int)
+    return $ ProgramState 0 arr [phase]  Nothing Nothing False
 
-runProgram :: [Int] -> InOut -> IO (Int, InOut)
-runProgram program inout = do
-   arr <- newListArray  (0, (length program)-1) program  :: IO (IOArray Int Int)
-   res <- processOpcodes 0 arr inout
-   return res
+mkAmps :: [Int] -> Maybe Int -> [Int] -> IO [ProgramState]
+mkAmps _ _ [] = return []
+mkAmps program initialInput (phase:phases) = do
+    amp <- mkAmp program phase
+    let initialAmp = case initialInput of
+                        Just input -> addInput amp input
+                        Nothing -> amp
+    restAmps <- mkAmps program Nothing phases
+    return (initialAmp:restAmps)
 
-
+addInput :: ProgramState -> Int -> ProgramState
+addInput state input = {- trace ( show ("adding input " ++ show input ++ " existing inputs: " ++ show (getInputs state))) -} state { getInputs = (getInputs state) ++ [input]}
 
 main = do
-   inputText <-readFile "puzzle07_input.txt"
-   let 
+    --inputText <- readFile "puzzle07_input.txt"
+    inputText <- readFile "puzzle07_input.txt"
+    let 
         program = fromJust . parseMaybe programParserP $ trace ("puzzle=" ++ show inputText) $ trim inputText
-        phaseCombinations = permutations [0..4]
+        phaseCombinations = permutations [5..9]
         
-        runCombo :: [Int] -> Int -> IO (Int)
-        runCombo [] lastProgramOutput = pure lastProgramOutput
-        runCombo (phaseInput:phaseInputs) lastProgramOutput = do
-            (_,(_,output)) <- runProgram program ([phaseInput,lastProgramOutput], Nothing)
-            runCombo phaseInputs (fromJust output)
-        
-        runCombos :: [[Int]] -> [([Int], Int)] -> IO [([Int],Int)]
-        runCombos [] comboResults = pure comboResults
-        runCombos (combo:combos) comboResults = do
-            comboRes <- runCombo combo 0
-            runCombos combos ((combo, comboRes):comboResults)
+        runAmps :: [ProgramState] -> IO Int
+        runAmps amps = do
+            let
+                replace pos newVal list = take pos list ++ newVal : drop (pos+1) list
 
-   comboResults <- runCombos phaseCombinations []
+                runAmpStep :: 
+                    [ProgramState] -> 
+                    Int -> -- which amp to run
+                    IO Int
+                runAmpStep states ampToRun = do
+                    newState <- processOpcodes (states!!ampToRun)
+                    let output = fromJust $ getOutput newState
+                        inputs = getInputs newState
+                        newStates = replace ampToRun newState states
+                        nextAmpToRun = case ampToRun == ((length states) -1) of
+                            True -> 0
+                            False -> ampToRun+1
+                        nextState = (newStates !! nextAmpToRun)
+                        newNextState = addInput nextState output -- chain output to input of next amp
+                        newPreppedStates = replace nextAmpToRun newNextState newStates
+                    case (isTerminated newState) of
+                        True -> return (head inputs) -- last input of terminated program was the output of the last
+                        False -> runAmpStep newPreppedStates nextAmpToRun
+            finalOutput <- runAmpStep amps 0
+            return finalOutput
 
+    ampConfigurations <- mapM (mkAmps program (Just 0)) phaseCombinations
+    res <- ( mapM runAmps ampConfigurations) :: IO [Int]
 
-   putStrLn ("program result: " ++ (show (maximumBy (comparing snd) comboResults)))
+    putStrLn ("program result: " ++ (show (maximumBy (comparing snd) (zip phaseCombinations res))))
