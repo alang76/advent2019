8b34b06054955284be5eec8300ed9e97d742b4a7
arjen <alangebaerd@gmail.com>
2019-12-11 20:55:11 +0100

    day 10 - part 1 complete

--- puzzle10/app/Main.hs
+++ puzzle10/app/Main.hs
@@ -29,7 +29,11 @@ data VisibilityMap = VisibilityMap {
 }
 
 instance Show VisibilityMap where
-    show vMap = concat . map (\row -> (concat row) ++ "\n") . chunksOf (vWidth vMap) . (map ((\val -> if val then "1" else "0") . snd)).  Map.toList $ (visibility vMap)
+    show vMap =
+        concat . map (\row -> concat row ++ "\n") . chunksOf (vWidth vMap) . map showCoord $ [(x,y) | y<-[0..4], x<-[0..4]]
+            where showCoord (x,y) = case fromJust $ Map.lookup (x,y) (visibility vMap) of
+                                False -> "0"
+                                True  -> "1"
 
 
 buildMap :: [[Location]] -> (AsteroidMap, VisibilityMap)
@@ -47,7 +51,7 @@ buildMap locations =
 
 
 blockLocation :: (Int, Int) -> VisibilityMap -> VisibilityMap
-blockLocation coord visMap = visMap {visibility = Map.insert coord False (visibility visMap)}
+blockLocation coord visMap = visMap {visibility = {- trace ("blocking: " ++ show coord) $ -} Map.insert coord False (visibility visMap)}
 
 isOnMap :: (Int,Int) -> VisibilityMap -> Bool
 isOnMap coord visMap
@@ -57,9 +61,9 @@ isOnMap coord visMap
         (x,y) = (fst coord, snd coord)
 
 blockRay :: Bool -> (Int, Int) -> Int -> Int -> VisibilityMap -> VisibilityMap
-blockRay blockStart start@(x,y) ratio_x ratio_y visMap =
+blockRay ignoreCurrent start@(x,y) ratio_x ratio_y visMap =
     let
-        blockedMap = if blockStart then visMap else blockLocation start visMap
+        blockedMap = if ignoreCurrent then visMap else blockLocation start visMap
     in
         case isOnMap start visMap of
             True -> blockRay False (x+ratio_x, y+ratio_y) ratio_x ratio_y blockedMap 
@@ -68,22 +72,21 @@ blockRay blockStart start@(x,y) ratio_x ratio_y visMap =
 updateVisibilityAsteroid :: (Int, Int) -> (Int,Int) -> VisibilityMap -> VisibilityMap
 updateVisibilityAsteroid pov lookAt visMap = 
     let
-
-
-        xdiff = fst pov - fst lookAt
-        ydiff = snd pov - snd lookAt
+        xdiff =  fst lookAt - fst pov
+        ydiff =  snd lookAt -  snd pov
         divisor = gcd xdiff ydiff
-        ratio_x = xdiff `div` ({- trace ("gcd for xdiff,ydiff " ++ show xdiff ++ ", " ++ show ydiff ++ " is " ++ show divisor) -} divisor)
+        ratio_x = xdiff `div` divisor
         ratio_y = ydiff `div` divisor
     in
-        blockRay True lookAt ratio_x ratio_y visMap
+        {- trace ("ratio_x = " ++ show ratio_x ++ ", ratio_y = " ++ show ratio_y) $ -}
+            blockRay True lookAt ratio_x ratio_y visMap
 
 updateVisibilityMap :: (Int, Int) -> [(Int, Int)] -> VisibilityMap -> VisibilityMap
 updateVisibilityMap _ [] visMap = visMap
 updateVisibilityMap pov (loc:astlocs) visMap = updateVisibilityMap pov astlocs (updateVisibilityAsteroid pov loc visMap)
 
 getAsteroidCoords :: AsteroidMap -> [(Int,Int)]
-getAsteroidCoords astMap = map fst . filter ((==) Asteroid . snd) . Map.toList $ locations astMap
+getAsteroidCoords astMap = map fst . filter ((==) Asteroid . snd) . Map.assocs $ locations astMap
 
 getLocation :: Char -> Location
 getLocation '.' = Empty
@@ -111,10 +114,11 @@ mapParserP = some $ do
 main :: IO ()
 main = do
     inputFile <- readFile "puzzle10_input.txt"
+    --inputFile <- readFile "test1.txt"
     let
         (asteroidMap, visibilityMap) = buildMap . fromJust . parseMaybe mapParserP $ inputFile
         asteroidCoords = trace ("ast coords = " ++ show (getAsteroidCoords asteroidMap)) getAsteroidCoords asteroidMap
-        asteroidsVisCount = map (\coord -> (countVisibleRoids visibilityMap asteroidCoords coord, coord)) asteroidCoords 
+        asteroidsVisCount = map (\coord -> (subtract 1 $ countVisibleRoids visibilityMap asteroidCoords coord, coord)) asteroidCoords 
         bestRoid = maximumBy (comparing fst) asteroidsVisCount
     print asteroidsVisCount
     print "Best roid is:"
